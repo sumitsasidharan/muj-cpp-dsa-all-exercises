8f6429eda484755d632aeb3159a386f2afc2919e
sumitsasidharan <s.sumit196@gmail.com>
2023-08-12 17:55:50 +0530

    renamed queues

---
+++ 04-Queues-Linked-List.cpp
@@ -0,0 +1,102 @@
+/************************************
+  Implement Queues using Linked List
+*************************************/
+
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+using namespace std;
+
+struct node {
+  int info;
+  struct node *next;
+};
+
+class Queue {
+  private:
+    node *rear;
+    node *front;
+
+  public:
+    Queue(){
+      rear = NULL;
+      front = NULL;
+    }
+    void Enqueue();
+    void Dequeue();
+    void display();
+
+};
+
+void Queue::Enqueue() {
+  int data;
+  node *temp = new node;
+  cout << "Enter the data to be inserted: ";
+  cin >> data;
+  temp->info = data;
+  temp->next = NULL;
+
+  // if front is NULL, Queue is empty
+  if (front == NULL) {
+    front = temp;
+  }
+  else {
+    rear->next = temp;
+  }
+}
+
+void Queue::Dequeue() {
+  node *temp = new node;
+  if (front == NULL) {
+    cout << "Queue is empty\n";
+  }
+  else {
+    temp = front;
+    front = front->next;
+    cout << "The data deleted from Queue is: " << temp->info;
+    delete temp;
+  }
+}
+
+void Queue::display() {
+  node *p = new node;
+  p = front;
+  if (front == NULL) {
+    cout << "Queue is empty\n";
+  }
+  else {
+    while(p != NULL) {
+      cout << "\nContents of Queue: ";
+      cout << p->info;
+      p = p->next;
+    }
+  }
+}
+
+
+int main() {
+  system("cls");
+  Queue q;
+  int choice;
+
+  while (1) {
+    cout << "\n1. Enqueue\n2. Dequeue\n3. Display\n4. Exit\n";
+    cout << "\nEnter your choice: ";
+    cin >> choice;
+
+    switch(choice) {
+      case 1: q.Enqueue();
+              break;
+      case 2: q.Dequeue();
+              break;
+      case 3: q.display();
+              break;
+      case 4: exit(0);
+      default:
+        cout << "\nSorry, your answer is incorrect. Please enter corret choice(1-4): \n";
+      
+    }
+  }
+
+  return 0;
+}
